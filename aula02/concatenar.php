c39aeef12a604b5ed6025a195bc6b2e02933e29d
Rafaela Sousa <sousarafaela2801@gmail.com>
2025-03-25 16:31:03 -0300

    aula sobre cocatenação, irei trancar o curso

---
+++ aula02/concatenar.php
@@ -0,0 +1,14 @@
+<?php
+ECHO "Oi mundinho <3 <br>";
+echo " oi mundo s2 <br>";
+EcHo "oi mundão ♡ <br>";
+
+$nome = "Lyria";
+$sobrenome ="Eckhart";
+$numero = 3;
+
+echo $nome ."comunista☭" . "<br>";
+echo "${nome} <h1> ${sobrenome} </h1>";
+echo $numero . "<p> " . $nome .
+" " . $sobrenome . "</p>";
+?> 
