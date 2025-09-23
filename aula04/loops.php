7fec2afc6f20adf7da7430b8873d988cd43339be
Lyria <sousarafaela2801@gmail.com>
2025-09-23 15:08:42 -0300

    loops

---
+++ aula04/loops.php
@@ -0,0 +1,24 @@
+<?php
+    echo "While <br>";
+    //while
+    $contador = 1;
+
+    while ($contador < 6) {
+        echo $contador, "<br>";
+        $contador++;
+    }
+    echo "Do While <br>";
+    //do while
+    $contadorii = 1;
+
+    do {
+        echo $contadorii , "<br>";
+        $contadorii++;
+    } while ($contadorii < 6);
+
+    echo "<br> For <br>";
+
+    for ($x = 0; $x <- 10; $x++) {
+        echo "O número é: $x <br>";
+    }
+?>
